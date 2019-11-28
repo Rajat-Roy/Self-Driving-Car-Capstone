c867fd1982a0522b9761c90340ede6fb645d5936
Rajat-Roy <rctbraj@gmail.com>
2019-11-29 00:50:29 +0530

    test

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 100 #100 Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 #100 Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5
 
 
@@ -57,7 +57,7 @@ class WaypointUpdater(object):
         
         
     def loop(self):
-        rate = rospy.Rate(20) #50
+        rate = rospy.Rate(30) #50
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 #Get closest waypoint
